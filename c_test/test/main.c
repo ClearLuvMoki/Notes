2f2b0dda90ca4c0d97280be888c8f061473fe43e
moki <2893096286@qq.com>
2021-09-08 16:07:17 +0800

    add

--- c_test/test/main.c
+++ c_test/test/main.c
@@ -1,32 +1,6 @@
 #include <stdio.h>
 int main()
 {
-    void inv(int *a, int n);
-    int a[10]= {1, 2, 3, 4, 5, 6,7 ,8 ,9 ,10}, i;
-    printf("输出换顺序之前的数组:\n");
-    for (i = 0; i < 10; i++)
-    {
-        printf("%d\t", a[i]);
-    }
-    printf("\n");
-    inv(a, 10);
-    printf("输出换顺序之后的数组:\n");
-    for (i = 0; i < 10; i++)
-    {
-        printf("%d\t", a[i]);
-    }
-    printf("\n");
+    printf("%d\n", 5>3);
     return 0;
-}
-
-void inv(int *arr, int n)
-{
-    int temp, i , j, m=(n-1)/2;
-    for (i = 0; i < m; i++){
-        j = n-1-i;
-        temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-    return;
 }
